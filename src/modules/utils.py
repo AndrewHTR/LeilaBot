b230cd07ef936df26c0a9a10964dbfdc2b9ce50b
Andrew Silva <andrewgameplays6@gmail.com>
2021-12-08 23:30:52 -0300

    modules

---
+++ src/modules/utils.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def get_guildid():
+    guild = os.getenv("guild_id")
+    if guild == None:
+        return os.environ["guild_id"]
+    return guild
+
+def get_token():
+    token = os.getenv("token")
+    if token == None:
+        return os.environ["token"]
+    return token
