07daf5a11d3013d49c5b6fb4f59151501333a943
Andrew Silva <andrewgameplays6@gmail.com>
2021-12-08 23:31:57 -0300

    iniciador main

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import discord
+from discord import file
+from discord.ext import commands
+from modules.utils import get_token
+import os
+
+intents = discord.Intents().all()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+for filename in os.listdir('./src/ent'):
+    if filename.endswith('.py'):
+        print(f'Loading entertaiment {filename}')
+        bot.load_extension(f'ent.{filename[:-3]}')
+for filename in os.listdir('./src/events'):
+    if filename.endswith('.py'):
+        print(f'Loading event {filename}')
+        bot.load_extension(f'events.{filename[:-3]}')
+for filename in os.listdir('./src/cogs'):
+    if filename.endswith('.py'):
+        print(f'Loading cogs {filename}')
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(get_token()) 
+
