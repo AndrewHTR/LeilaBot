a267acb10d493d99e19d9d366e79ee90c72f0f2d
Andrew Silva <andrewgameplays6@gmail.com>
2022-04-15 15:29:20 -0300

    changed music commands to a more complete way

--- src/entertaiment/music.py
+++ src/entertaiment/music.py
@@ -7,7 +7,7 @@ import random
 import discord
 import youtube_dl
 from async_timeout import timeout
-from discord.ext import commands
+from discord.ext import commands, bridge
 
 # Silence useless bug reports messages
 youtube_dl.utils.bug_reports_message = lambda: ''
