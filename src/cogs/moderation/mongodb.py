b0954213dc4537219f53276b0e63958b1eede2d3
AndrewHTR <andrewgameplays6@gmail.com>
2023-01-23 02:49:06 -0300

    fixing

--- src/cogs/moderation/mongodb.py
+++ src/cogs/moderation/mongodb.py
@@ -6,8 +6,6 @@ try:
     cluster = MongoClient(modules.utils.get_uri())
     db = cluster["LeilaBot"]
     collection = db["UserData"]
-    post = {"_id": 2, "score": 1}
-    collection.insert_one(post)
 except:
     print("sexo")
 
